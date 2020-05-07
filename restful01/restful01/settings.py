2910b51c49c49ad043a2e800d7341f5614eeac39
vr3n <vickyspatel@gmail.com>
2020-05-07 16:02:16 +0530

    -

--- restful01/restful01/settings.py
+++ restful01/restful01/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'toys.apps.ToysConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
